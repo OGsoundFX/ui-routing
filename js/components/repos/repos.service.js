b9c79765b2753aacebebf4fb6f21782a9a7478ad
Olivier Girardot <ogmusik@gmail.com>
2020-09-16 15:23:11 +0200

    added a factory to compare with a service

--- js/components/repos/repos.service.js
+++ js/components/repos/repos.service.js
@@ -6,8 +6,20 @@ function ReposService ($http) {
                 return response.data;
             });
     };
+
+// Same result but using factory instead of service:
+    // return {    
+    //     getRepos: function (entry) {
+    //         return $http
+    //         .get(`https://api.github.com/users/${entry}/repos`)
+    //         .then(function (response) {
+    //             return response.data;
+    //         });
+    //     }
+    // };
 };
 
 angular
     .module('repos')
     .service('ReposService', ReposService)
+    // .factory('ReposService', ReposService)
